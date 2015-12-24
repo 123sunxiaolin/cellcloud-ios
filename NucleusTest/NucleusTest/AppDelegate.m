2031bb48ffee90eb5c6890b060966c95ad786ac8
zhuguoqiang <zhuguoqiang>
2015-12-24 16:21:04 +0800

    Foreground clear keepAliveTimer

--- NucleusTest/NucleusTest/AppDelegate.m
+++ NucleusTest/NucleusTest/AppDelegate.m
@@ -66,6 +66,8 @@
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     NSLog(@"applicationWillEnterForeground");
+    
+    [[UIApplication sharedApplication] clearKeepAliveTimeout];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
